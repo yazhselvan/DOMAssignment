e93cdd0a8587eed9baa788f4ee7817f5f2d6f112
yazhselvan <yazhslvn89@gmail.com>
2024-04-20 12:18:59 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function address(){
+    var address = {
+        "First name": document.getElementById("firstName").value,
+        "Last name": document.getElementById("lastName").value,
+        "Email": document.getElementById("email").value,
+        "Address": document.getElementById("address").value
+    }
+    console.log(address);
+}
